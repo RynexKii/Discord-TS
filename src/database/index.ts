b5c08c12182f64535ce7899cf13b683785f27850
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-09-04 23:36:01 -0300

    Feat: Adicionado novo Schema para a Guild no Banco de Dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,7 @@ import mongoose, { model } from "mongoose";
 import { log } from "#settings";
 import chalk from "chalk";
 import { profileSchema } from "./schemas/profile.js";
+import { guildSchema } from "./schemas/guild.js";
 
 // Conectando no MongoDB
 try {
@@ -14,5 +15,6 @@ try {
 
 // Criando o model e exportando
 export const database = {
+    guild: model("Guild", guildSchema, "Guild"),
     profile: model("Profile", profileSchema, "Profile"),
 };
---
+++ src/database/schemas/guild.ts
@@ -0,0 +1,58 @@
+import { Schema } from "mongoose";
+
+export const guildSchema = new Schema(
+    {
+        guildId: { type: String, required: true },
+    },
+    {
+        versionKey: false,
+        statics: {
+            // Set = Para setar um valor na chave no Banco de Dados
+            async set(guildId: string, key: string, value: number | string) {
+                let guild = await this.findOne({ guildId: guildId });
+
+                if (!guild) guild = await this.create({ guildId: guildId });
+
+                await guild.updateOne({ [key]: value });
+
+                guild = await this.findOne({ guildId: guildId });
+
+                return guild?.get(key);
+            },
+            // Delete = Para deletar um perfil do Banco de Dados
+            async delete(guildId: string) {
+                await this.findOneAndDelete({ guildId: guildId });
+            },
+            // Get = Para retornar alguma chave do Banco de Dados
+            async get(guildId: string, key: string) {
+                const guild = await this.findOne({ guildId: guildId });
+
+                return guild?.get(key);
+            },
+            // Add = Para adicionar valor a chave no Banco de Dados
+            async add(guildId: string, key: string, value: number) {
+                let guild = await this.findOne({ guildId: guildId });
+
+                if (!guild) guild = await this.create({ guildId: guildId });
+
+                await guild.updateOne({ [key]: guild.get(key) + value });
+
+                guild = await this.findOne({ guildId: guildId });
+
+                return guild?.get(key);
+            },
+            // Sub = Para subtrair valor da chave no Banco de Dados
+            async sub(guildId: string, key: string, value: number) {
+                let guild = await this.findOne({ guildId: guildId });
+
+                if (!guild) guild = await this.create({ guildId: guildId });
+
+                await guild.updateOne({ [key]: guild.get(key) - value });
+
+                guild = await this.findOne({ guildId: guildId });
+
+                return guild?.get(key);
+            },
+        },
+    }
+);
